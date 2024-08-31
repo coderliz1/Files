1ed3aa68a2f70b6349bfec8fb19c4eec9df5e3ee
Lizzy <coderliz1@gmail.com>
2024-08-31 17:55:58 -0400

    calebs python back end

--- files/views.py
+++ files/views.py
@@ -1,28 +1,48 @@
 from django.http import Http404 
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import File
 
 
 def index(request):
     return render(request, 'files/index.html')
 
-data = [
-    {'id': 0, 'name': 'image1.jpg', 'type':'jpg'},
-    {'id': 1, 'name': 'notes.txt', 'type':'txt'},
-    {'id': 2, 'name': 'image2.jpg', 'type': 'jpg'}
 
-]
+
 
 
 
 def files(request):
+    data =File.objects.all()
     return render(request, 'files/files.html', {'files': data})
 
 def file(request, file_id):
-    print('File ID:', file_id)
-    f = next((item for item in data if item.get('id') == file_id), None)
+    f = File.objects.get(pk=file_id)
     if f:
         return render(request, 'files/file.html', {'file': f})
     else:
         raise Http404('File does not exist.')
     
     
+
+def edit(request, file_id):
+    name = request.POST.get('name')
+    file_type = request.POST.get('type')
+    f = File.objects.get(pk=file_id)
+    print(name, file_type, f)
+
+    if f:
+        if name:
+            f.name = name
+        if file_type:
+            f.file_type = file_type
+        f.save()
+        return redirect(files)
+    else:
+        return redirect(files)
+    
+
+def delete(request, file_id):
+    f = File.objects.get(pk=file_id)
+    if f:
+        f.delete()
+    return redirect(files)

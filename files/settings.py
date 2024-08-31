dad44a62f8986c2198f62006f34e6fc82c322b78
Lizzy <coderliz1@gmail.com>
2024-08-31 11:05:36 -0400

    calebs back end python

--- files/settings.py
+++ files/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wbz@(!s_^pvlvxfm_m9@rhm43nl$d^jk)@sm!jfd=9p&*m^)dh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition

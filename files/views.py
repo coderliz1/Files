7e324f8e02f35f604eb5ad51a49d870ea857bca9
Lizzy <coderliz1@gmail.com>
2024-09-01 12:21:03 -0400

    calebs back end python

--- files/views.py
+++ files/views.py
@@ -1,5 +1,7 @@
 from django.http import Http404 
 from django.shortcuts import redirect, render
+
+from files.forms import UploadForm
 from .models import File
 
 
@@ -13,7 +15,7 @@ def index(request):
 
 def files(request):
     data =File.objects.all()
-    return render(request, 'files/files.html', {'files': data})
+    return render(request, 'files/files.html', {'files': data, 'form':UploadForm})
 
 def file(request, file_id):
     f = File.objects.get(pk=file_id)
@@ -45,4 +47,10 @@ def delete(request, file_id):
     f = File.objects.get(pk=file_id)
     if f:
         f.delete()
+    return redirect(files)
+
+def upload(request):
+    form = UploadForm(request.POST, request.FILES)
+    if form.is_valid():
+        form.save()
     return redirect(files)
